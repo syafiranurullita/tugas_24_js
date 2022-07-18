f79c89a59cec871e7ee48e3a6c75a9b7dac8c1f2
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 20:08:19 +0700

    tugas 24 js
    untuk niomic

---
+++ Tugas24/Tugas24.js
@@ -0,0 +1,13 @@
+function panggilSort(){
+  var kota = [32,33,16,40,9,7,11,2,44,1,66]
+  console.log(kota);
+  console.log("=====================================================");
+  kota = kota.sort();
+
+  console.log(kota);
+  kota = kota.reverse();
+
+  return kota
+
+}
+console.log(panggilSort()); //untuk mengurutkan sesuai kata sesuai abjad
